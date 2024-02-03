ce90f560245da3d0d6b302158d2829d7035d4076
VINITHJI R <153160710+VINITHJIR@users.noreply.github.com>
2024-02-03 13:58:44 +0530

    Frontend src Folder Commited

---
+++ frontend/src/assets/data/admin.js
@@ -0,0 +1,22 @@
+import AdminImg01 from "../images/admin-img01.png";
+import AdminImg02 from "../images/admin-img03.png";
+const admin = [
+          {
+                    id: "01",
+                    title: "profile",
+                    photo: AdminImg01,
+                    path: '/admin_profile'
+
+          },
+
+          {
+                    id: "02",
+                    title: "Client Transaction ",
+                    photo: AdminImg02,
+                    path: '/client_trasac'
+          },
+
+
+]
+
+export default admin;
